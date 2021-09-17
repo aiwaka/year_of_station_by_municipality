06ec9777822ed53eb9f89db9e3b83ec9c90b6ca3
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-09-17 13:58:28 +0900

    add auto logging function

--- error_storage.py
+++ error_storage.py
@@ -1,8 +1,19 @@
+from logzero import logger
+
+
 class ErrorStorage:
     def __init__(self) -> None:
         self.storage = []
 
-    def add(self, content):
+    def add(self, content, log: str = ""):
+        # log引数を付けておくと自動でログ出力もされる.
+        if log:
+            if log == "i":
+                logger.info(content)
+            elif log == "w":
+                logger.warning(content)
+            elif log == "e":
+                logger.error(content)
         self.storage.append(content)
 
 
