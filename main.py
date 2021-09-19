8c7b0f5c7f8d46274371bff80496a4b8192c6222
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-09-19 16:44:17 +0900

    update data

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ logfile("log.log", disableStderrLogger=False)
 
 def main() -> None:
     config = {
-        "START_INDEX": 10,  # 検索開始するインデックス
-        "GET_NUM": 5,  # データを取得する最大数. 最大ならlen(man_list)でOK
+        "START_INDEX": 0,  # 検索開始するインデックス
+        "GET_NUM": 1900,  # データを取得する最大数. 指定しなければすべて取得する.
     }
     collector = Collector(config)
     collector.run()
